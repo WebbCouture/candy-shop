e3d48b7c6e4d22c28cfad55ecd596b63dc38889d
WebbCouture <lesnik.amanda@icloud.com>
2025-07-31 10:43:17 +0200

    fix path to other pages

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Product  # Only needed if you have a Product model
 
 def home(request):
     return render(request, 'main/home.html')
+
+def product_list(request):
+    # If you don't have a Product model yet, just pass a dummy list for now
+    # Example: products = [{'name': 'Candy A', 'price': 2.99}, ...]
+    return render(request, 'main/product_list.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
